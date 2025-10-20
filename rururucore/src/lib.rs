df53b334e92656dee31327861df36402b8e5ecf6
Michel Boto <michel.boto@gmail.com>
2025-10-20 10:38:09 +0200

    implement/test language_info

--- rururucore/src/lib.rs
+++ rururucore/src/lib.rs
@@ -62,4 +62,38 @@ mod tests {
         let result: Result<KernelSpec> = serde_json::from_str(data_missing_display_name);
         assert!(result.is_err());
     }
+
+    // see nbformat-schema.json line 31
+    #[test]
+    fn test_language_info() {
+        let data = r#"
+        {
+                "name": "python",
+                "codemirror_mode": "ipython",
+                "mimetype": "text/x-python",
+                "file_extension": ".py",
+                "pygments_lexer": "ipython3"    
+    }
+        "#;
+
+        let lang_info: LanguageInfo = serde_json::from_str(data).unwrap();
+        assert_eq!(lang_info.name, "python");
+        assert_eq!(lang_info.codemirror_mode.unwrap(), "ipython");
+        assert_eq!(lang_info.mimetype.unwrap(), "text/x-python");
+        assert_eq!(lang_info.file_extension.unwrap(), ".py");
+        assert_eq!(lang_info.pygments_lexer.unwrap(), "ipython3");
+
+        // test that name is required
+        let data_missing_name = r#"
+        {
+                "codemirror_mode": "ipython"
+                "mimetype": "text/x-python"
+                "file_extension": ".py"
+                "pygments_lexer": "ipython3"
+    }
+        "#;
+
+        let result: Result<LanguageInfo> = serde_json::from_str(data_missing_name);
+        assert!(result.is_err());
+    }
 }
